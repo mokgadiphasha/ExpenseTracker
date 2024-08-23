0d380c78183f7e55ada38cba1608f860c7fd1172
mophash022 <mophash022@student.wethinkcode.co.za>
2024-08-23 12:45:41 +0200

    fixed the errors and everything at this moment works well , i have to add error handling mechanisms

--- src/main/java/com/example/ExpenseTracker/Service/Financial_Insights/Expense_Summary/ExpenseSummaryHelper.java
+++
@@ -1,9 +0,0 @@
-package com.example.ExpenseTracker.Service.Financial_Insights.Expense_Summary;
-
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.util.List;
-
-public interface ExpenseSummaryHelper {
-    List<YearMonth> getMonthsBetween(LocalDate start,LocalDate end);
-}
